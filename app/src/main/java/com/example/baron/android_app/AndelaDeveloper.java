7664f29256626008acf80ee8a0e5bfe06cd7ecfc
ivanatu <ivanatu@gmail.com>
2018-03-13 19:33:56 +0300

    [ft #155889952] Add detailed view

---
+++ app/src/main/java/com/example/baron/android_app/AndelaDeveloper.java
@@ -0,0 +1,35 @@
+package com.example.baron.android_app;
+
+/**
+ * Created by baron on 07/03/2018.
+ */
+
+public class AndelaDeveloper{
+
+    private String github, location;
+
+    public AndelaDeveloper() {
+    }
+
+    public AndelaDeveloper( String github, String location) {
+        this.github = github;
+        this.location = location;
+    }
+
+
+    public String getlocation() {
+        return location;
+    }
+
+    public void setlocation(String location) {
+        this.location = location;
+    }
+
+    public String getgithub() {
+        return github;
+    }
+
+    public void setgithub(String github) {
+        this.github = github;
+    }
+}
---
+++ app/src/main/java/com/example/baron/android_app/MyAdapter.java
@@ -0,0 +1,93 @@
+package com.example.baron.android_app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private ArrayList<AndelaDeveloper> developers;
+    private Context context;
+
+
+    public MyAdapter(ArrayList<AndelaDeveloper> developers, Context context) {
+        this.developers = developers;
+        this.context = context;
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.content_main, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final AndelaDeveloper andeladeveloper = developers.get(position);
+
+        holder.github.setText(andeladeveloper.getgithub());
+        holder.location.setText(andeladeveloper.getlocation());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent detailIntent = new Intent(v.getContext(), DisplayMessageActivity.class);
+              detailIntent.putExtra("github", andeladeveloper.getgithub());
+              detailIntent.putExtra("location", andeladeveloper.getlocation());
+//              detailIntent.putExtra("GITURL", gitUrl);
+              v.getContext().startActivity(detailIntent);
+          }
+       });
+    }
+
+    @Override
+    public int getItemCount() {
+        return developers.size();
+    }
+
+    private void openDetailActivity(String github,String location)
+
+    {
+        Intent i=new Intent(context, DisplayMessageActivity.class);
+
+        //PACK DATA TO SEND
+        i.putExtra("github",github);
+        i.putExtra("location", location);
+//        i.putExtra("IMAGE_KEY",image);
+
+        //open activity
+        context.startActivity(i);
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public  ImageView imageView;
+        public  TextView github;
+        public  TextView location;
+
+//        String githubtext;
+//        String locationtext;
+//        Drawable image;
+
+        public ViewHolder(View view) {
+            super(view);
+            imageView = (ImageView) view.findViewById(R.id.imageView);
+            github = (TextView) view.findViewById(R.id.github);
+            location = (TextView) view.findViewById(R.id.location);
+        }
+
+
+//    @Override
+//    public String toString() {
+//            return super.toString() + " '" + mContentView.getText() + "'";
+//        }
+    }
+}
