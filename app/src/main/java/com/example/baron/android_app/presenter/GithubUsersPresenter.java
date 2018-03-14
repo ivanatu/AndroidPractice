1675e6d365e0655392504da26ce023c1306566d0
ivanatu <ivanatu@gmail.com>
2018-03-14 11:17:33 +0300

    [ft #155889952] Creating classes for presenter

--- app/src/main/java/com/example/baron/android_app/presenter/GithubUsersPresenter.java
+++ app/src/main/java/com/example/baron/android_app/presenter/GithubUsersPresenter.java
@@ -1,8 +1,47 @@
 package com.example.baron.android_app.presenter;
 
+import com.example.baron.android_app.model.GithubUsers;
+import com.example.baron.android_app.service.GithubService;
+
+import java.util.List;
+
 /**
  * Created by baron on 13/03/2018.
  */
 
 public class GithubUsersPresenter {
+
+    private GithubService githubService;
+
+    private GithubUsersPresenter(){
+        if(this.githubService == null){
+            this.githubService = new GithubService();
+        }
+    }
+
+    public void getUsers(){
+        githubService
+                .getAPI()
+                .gettotalcount()
+                .enqueue(new Callback<Data>(){
+                    @Override
+                    public void onResponse(){
+                        Data data = response.body();
+
+                        if (data != null && data.getRestResponse() != null) {
+                            List<GithubUsers> result = data.getRestResponse().getResult();
+                            countryView.countriesReady(result);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Data> call, Throwable t) {
+                        try {
+                            throw new InterruptedException("Something went wrong!");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+    }
 }
