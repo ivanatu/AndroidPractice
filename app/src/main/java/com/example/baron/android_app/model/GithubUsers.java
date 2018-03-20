89f58a6707b505d6e8190ec4c3541abd404f06d5
ivanatu <ivanatu@gmail.com>
2018-03-20 22:45:52 +0300

    [ft #156005187] Implement for the parcable in github users

--- app/src/main/java/com/example/baron/android_app/model/GithubUsers.java
+++ app/src/main/java/com/example/baron/android_app/model/GithubUsers.java
@@ -1,11 +1,15 @@
 package com.example.baron.android_app.model;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+
 import com.google.gson.annotations.SerializedName;
 
 /**
  * Created by baron on 12/03/2018.
  */
 
-public class GithubUsers {
+public class GithubUsers implements Parcelable{
     @SerializedName("login") // from github
     private String username; // your custom name
 
@@ -21,6 +25,25 @@ public class GithubUsers {
         this.username=username;
         this.github=github;
     }
+
+    protected GithubUsers(Parcel in) {
+        username = in.readString();
+        profileImage = in.readString();
+        github = in.readString();
+    }
+
+    public static final Creator<GithubUsers> CREATOR = new Creator<GithubUsers>() {
+        @Override
+        public GithubUsers createFromParcel(Parcel in) {
+            return new GithubUsers(in);
+        }
+
+        @Override
+        public GithubUsers[] newArray(int size) {
+            return new GithubUsers[size];
+        }
+    };
+
     public String getProfileImage() {
         return profileImage;
     }
@@ -34,5 +57,15 @@ public class GithubUsers {
     }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(username);
+        parcel.writeString(profileImage);
+        parcel.writeString(github);
+    }
 }
