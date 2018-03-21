0ceb7e4c5584641b22a51d5f2e1045901165da26
ivanatu <ivanatu@gmail.com>
2018-03-21 23:18:42 +0300

    [ft #156005187] Implement save state and restore state functions

--- app/src/main/java/com/example/baron/android_app/view/MainActivity.java
+++ app/src/main/java/com/example/baron/android_app/view/MainActivity.java
@@ -15,20 +15,19 @@ import android.widget.Toast;
 import com.example.baron.android_app.R;
 import com.example.baron.android_app.model.GithubUsers;
 import com.example.baron.android_app.presenter.GithubUsersPresenter;
-
-import java.io.Serializable;
 import java.util.ArrayList;
 
 import adapter.GithubAdapter;
 
 
-public class MainActivity extends AppCompatActivity implements Serializable{
+public class MainActivity extends AppCompatActivity implements GithubUsersPresenter.ViewGithubUsers{
     RecyclerView mRecyclerView;
     TextView Disconnected;
 
     private RecyclerView.Adapter mAdapter;
     RecyclerView.LayoutManager mLayoutManager;
-    private static ArrayList<GithubUsers> developers = new ArrayList<>();
+    ArrayList<GithubUsers> developers = new ArrayList<>();
+
     Parcelable list;
     private GithubUsersPresenter presenter = new GithubUsersPresenter(MainActivity.this);
     public final static String LIST_KEY = "list_state";
@@ -39,25 +38,65 @@ public class MainActivity extends AppCompatActivity implements Serializable{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
+
+        pd = new ProgressDialog(this);
+        pd.setMessage("Fetching Github Users...");
+        pd.setCancelable(false);
+
+        displayGithubUsers(developers);
+
+        swipeRefreshLayout = findViewById(R.id.swipe);
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_orange_dark);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Disconnected = findViewById(R.id.disconnected);
+                try{
+                    presenter.getUsers(MainActivity.this);
+                    Toast.makeText(MainActivity.this, "Github users Refreshed",
+                            Toast.LENGTH_SHORT).show();
+                    swipeRefreshLayout.setRefreshing(false);
+                }catch(Exception e ){
+                    Log.d("Error", e.getMessage());
+                    Disconnected.setVisibility(mRecyclerView.VISIBLE);
+                    Toast.makeText(MainActivity.this, "Error Fetching data",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         if (savedInstanceState != null && savedInstanceState.containsKey(LIST_KEY)) {
-            onRestoreInstanceState(savedInstanceState); // Restore data found in the Bundle
-            mRecyclerView.scrollToPosition(savedInstanceState.getInt("position"));
+           onRestoreInstanceState(savedInstanceState); // Restore data found in the Bundle
         }
         else {
             pd.show();
-            presenter.getUsers(mRecyclerView);
+            presenter.getUsers(this);
             pd.dismiss();
         }
 
     }
 
-    private void initViews() {
-        pd = new ProgressDialog(this);
-        pd.setMessage("Fetching Github Users...");
-        pd.setCancelable(false);
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putParcelableArrayList(LIST_KEY, developers);
+    }
 
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            developers = savedInstanceState.getParcelableArrayList(LIST_KEY);
+            displayRecycleView(developers);
+        }
+    }
+    public void displayRecycleView(ArrayList<GithubUsers> users){
+        RecyclerView.Adapter adapter = new GithubAdapter(users, this);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void displayGithubUsers(ArrayList<GithubUsers> users) {
+        developers=users;
         mRecyclerView = findViewById(R.id.list);
 
         // use this setting to improve performance if you know that changes
@@ -67,57 +106,9 @@ public class MainActivity extends AppCompatActivity implements Serializable{
         // use a linear layout manager
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // specify an adapter (see also next example)
-//        mAdapter = new GithubAdapter(developers, this);
-//        mRecyclerView.setAdapter(mAdapter);
-//        presenter.getUsers(mRecyclerView);
-
-        swipeRefreshLayout = findViewById(R.id.swipe);
-        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_orange_dark);
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                Disconnected = findViewById(R.id.disconnected);
-                try{
-                    presenter.getUsers(mRecyclerView);
-                    swipeRefreshLayout.setRefreshing(false);
-                }catch(Exception e ){
-                    Log.d("Error", e.getMessage());
-                    Disconnected.setVisibility(mRecyclerView.VISIBLE);
-                    Toast.makeText(MainActivity.this, "Error Fetching data", Toast.LENGTH_SHORT).show();
-                }
-                Toast.makeText(MainActivity.this, "Github users Refreshed", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        list = mRecyclerView.getLayoutManager().onSaveInstanceState();
-//        outState.putInt("position", mRecyclerView.getLayoutPosition());
-        outState.putParcelableArrayList(LIST_KEY, developers);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        if (savedInstanceState != null) {
-            developers = savedInstanceState.getParcelableArrayList(LIST_KEY);
-            displayRecycleView(developers);
-
-        }
-        super.onRestoreInstanceState(savedInstanceState);
-    }
-    public void displayRecycleView(ArrayList<GithubUsers> users){
         RecyclerView.Adapter adapter = new GithubAdapter(users, this);
         mRecyclerView.setAdapter(adapter);
+
     }
-
-//    private void loadJSON() {
-//
-//    }
-
 
 }
